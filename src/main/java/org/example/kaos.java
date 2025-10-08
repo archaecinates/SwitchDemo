2aa9dbb767c35b53729399f97f95dc6096fd2221
nadanadifaramaeyza <nadanadifaramaeyza@gmail.com>
2025-10-08 19:24:13 +0700

    menambahkan komentar

--- src/main/java/org/example/kaos.java
+++ src/main/java/org/example/kaos.java
@@ -9,29 +9,29 @@ public class kaos { //mendeklarasikan kelas utama yakni kaos
         String ukuran = ""; //mendeklarasikan ukuran dengan tipe String
         int harga = 0, total, jumlah = 0; //mendeklarasikan harga, total, dan jumlah dengan tipe int
 
-        System.out.print("masukkan ukuran: ");
-        ukuran=Kaos.nextLine();
-        System.out.print("masukkan jumlah: ");
-        jumlah=Kaos.nextInt();
+        System.out.print("masukkan ukuran: "); //input ukuran
+        ukuran=Kaos.nextLine(); //baca ukuran
+        System.out.print("masukkan jumlah: "); //input jumlah
+        jumlah=Kaos.nextInt(); //baca jumlah
 
         switch (ukuran){ //mengevaluasi ekspresi ukuran
             case "S": //ekspresi adalah S
-                harga=30000; //value adalah 30000
-                break; //berhenti paksa
-            case "M":
-                harga=38000;
-                break;
-            case "L":
-                harga=45000;
-                break;
-            case "XL":
-                harga=50000;
-                break;
-            case "XXL":
-                harga=60000;
-                break;
+                harga=30000; //S memiliki harga 30000
+                break; //menghentikan secara paksa proses switch setelah case cocok
+            case "M": //ekspresi adalah M
+                harga=38000; //M memiliki harga 38000
+                break; //menghentikan secara paksa proses switch setelah case cocok
+            case "L": //ekspresi adalah L
+                harga=45000; //L mimiliki harga 45000
+                break; //menghentikan secara paksa proses switch setelah case cocok
+            case "XL": //ekpresi adalah XL
+                harga=50000; //Xl memiliki harga 50000
+                break; //menghentikan secara paksa proses switch setelah case cocok
+            case "XXL": //ekspresi adalah XXL
+                harga=60000; //XXL memiliki harga 60000
+                break; //menghentikan secara paksa proses switch setelah case cocok
             default: ukuran = "invalid"; //mengembalikan jika tidak ada yang valid
-            break;
+            break; //menghentikan secara paksa proses switch setelah case cocok
         }
 
         total = jumlah * harga; //menghitung total dengan rumus jumlah * harga
